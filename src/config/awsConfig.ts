f748125f788b04ce6f14f7eb7b81022dae11217f
Mirul Ansari <zaps@mailinator.com>
2024-08-01 13:28:27 +0530

    aws config secured and constantFunction file created

--- src/config/awsConfig.ts
+++ src/config/awsConfig.ts
@@ -1,24 +1,9 @@
-// Dev
-// export const awsConfig = {
-//     identityPoolId: '',
-//     region: 'ap-south-1',
-//     userPoolId: 'ap-south-1_Yiuj6zWlg',
-//     userPoolWebClientId: '6j6nbv1u87t6hr7f1v4grktpnh'
-// };
-
-// QA
-// export const awsConfig = {
-//   identityPoolId: "",
-//   region: "ap-south-1",
-//   userPoolId: "ap-south-1_7yixJxl2d",
-//   userPoolWebClientId: "6tnhgr9bld7tvinc61get3t4qj",
-// };
 
 export const awsConfig = {
   identityPoolId: '',
   region: 'ap-south-1',
-  userPoolId: 'ap-south-1_CWVaca2JU',
-  userPoolWebClientId: '5c7l8lmnd6l4odk19u0d3omup1',
+  userPoolId: process.env.VITE_USER_POOL_ID,
+  userPoolWebClientId: process.env.VITE_CLIENT_ID,
 };
 
 export const OTP_CELL_COUNT = 6;
